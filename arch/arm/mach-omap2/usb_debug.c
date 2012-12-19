c365edf1dc20d57af86a1c6baaf6e5c809ee7c4a
Tianfu Ma <matianfu@gmail.com>
2012-12-19 19:21:23 +0800

    keep playing with usb...

---
+++ arch/arm/mach-omap2/usb_debug.c
@@ -0,0 +1,7 @@
+#ifndef OMAP3_USB_DEBUG
+#define OMAP3_USB_DEBUG
+
+#include <plat/usb.h>
+
+
+#endif
---
+++ arch/arm/plat-omap/include/plat/omap_usb_debug.h
@@ -0,0 +1,8 @@
+#ifndef OMAP_USB_DEBUG_H_
+#define OMAP_USB_DEBUG_H_
+
+#include <plat/usb.h>
+
+
+
+#endif
